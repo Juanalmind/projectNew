a00a02e25fb3c98ad947488a571ffd9f488d7e16
Bernabé <berna.gar@hotmail.com>
2018-01-08 13:43:21 +0100

    el jodido JTA

---
+++ src/java/controller/helloController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author A685792
+ */
+
+@Controller
+@RequestMapping("/say")
+public class helloController {
+
+  @RequestMapping(value = "/hello", method = RequestMethod.GET)
+  public String hello(){
+    return "helloWorld.jsp";
+  }
+
+  @RequestMapping(value = "/helloMAV", method = RequestMethod.GET)
+  public ModelAndView helloMAV(){
+    return new ModelAndView("helloWorld.jsp");
+  }
+
+
+}
